95d1b90913864a621144123112bfff891b71d290
jonnyjakobsson <jonnyjakobsson@gmail.com>
2015-08-26 11:11:45 +0200

    Updated main with name question

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		String name = "";
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println("What is your name?");
+			name = bf.readLine();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	
+		System.out.println("HelloWorld " + name);
 	}
 
 }
